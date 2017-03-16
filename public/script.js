3f6d7f2c947fce74225164f3b4a165d6dde2df41
samsmith453 <Sam@admin.home>
2017-03-16 21:37:27 +0000

    Big commit

--- public/script.js
+++ public/script.js
@@ -1,16 +1,55 @@
-var socket = io.connect("192.168.0.16:3000");
+var socket = io.connect(window.location.hostname);
+//will need to change the above to work on heroku
+
+var lastsender;
 
 function receiveMesg(msg){
-   console.log("receive");
-   $(".chatbox").append("They said: "+msg+"<BR>");
+   var text = msg.msg;
+   var sender = msg.sender;
+   var time = msg.time;
+   if(sender==lastsender){
+      console.log("yes");
+      $(".chatbox").append("<div class='row'><div class='filler'></div><p class='sender'></p><p class='msg'>"+text+"</p><pclass='time'>"+time+"</p></div>")
+   }
+   else if(sender==person){
+      $(".chatbox").append("<div class='row'><img class='pic' src='/pics/"+sender+".jpg'><p class='sender'>Me</p><p class='msg'>"+text+"</p><pclass='time'>"+time+"</p></div>")
+   }
+   else{
+      $(".chatbox").append("<div class='row'><img class='pic' src='/pics/"+sender+".jpg'><p class='sender'>"+sender+"</p><p class='msg'>"+text+"</p><pclass='time'>"+time+"</p></div>")
+   }
+
+   lastsender=sender;
+
+}
+
+function scrolldown(){
+   $('html, body').animate({scrollTop:$(document).height()}, 'slow');
 }
 
 $(document).ready(function(){
+scrolldown();
+   $(".chatbox").on("mouseenter", ".row", function(){
+      $(this).css({"background-color": "#c6d3ff"});
+   });
+   $(".chatbox").on("mouseleave", ".row", function(){
+      $(this).css({"background-color": "white"});
+   });
+
    $(".btn").click(function(){
       var val = $(".textbox").val();
-      $(".chatbox").append("I said: " +val+"<BR>");
-      socket.emit("inbound", val);
-      $(".textbox").val("");
+      if(val!=""){
+            var d = new Date();
+            var time = d.getHours() +":"+ d.getMinutes() + " " + d.getDate() +"/"+(d.getMonth()+1);
+         var obj = {
+            msg: val,
+            sender: person,
+            time: time
+         }
+         socket.emit("inbound", obj);
+         $(".textbox").val("");
+         scrolldown();
+      }
+      return;
    })
 
    socket.on("outbound", receiveMesg);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var socket = require("socket.io");
 var app = express();
 var server = app.listen(process.env.PORT);
 var io = socket(server);
+var fs = require("fs");
 
 var auth = require("http-auth");
 var digest = auth.digest({
@@ -14,9 +15,27 @@ var digest = auth.digest({
 
 app.use(auth.connect(digest));
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/", function(req, res){
-   app.use(express.static("public"));
-   res.sendFile(__dirname + "/public/index.html");
+   fs.readFile("history.json", function(err, data){
+      if(err) throw err;
+      var obj = JSON.parse(data);
+      var arr = obj.chat;
+      var record = [];
+      for(var i=1; i<arr.length; i++){
+         if(arr[i].sender==arr[i-1].sender) record.push(i);
+      }
+      for(var h=0; h<record.length; h++){
+         var n = record[h];
+         arr[n].sender="";
+      }
+
+      app.use(express.static("public"));
+      res.render('index', { user: req.user, arr: arr });
+   })
+
 });
 
 io.sockets.on("connection", newConnection);
@@ -24,6 +43,27 @@ io.sockets.on("connection", newConnection);
 function newConnection(socket){
    socket.on("inbound", inboundMessage);
    function inboundMessage(data){
-      socket.broadcast.emit("outbound", data);
+      io.sockets.emit("outbound", data);
+      files(data);
    }
+}
+
+function files(msg){
+
+   fs.readFile("history.json", function(err, data){
+      if(err) throw err;
+      var obj = JSON.parse(data);
+      var arr = obj.chat;
+      var arrlength = arr.length;
+      arr.push(msg);
+
+      if(arrlength>100) arr.shift();
+
+      var newobj = {"chat":arr};
+      var newfile = JSON.stringify(newobj, null, 2);
+      fs.writeFile("history.json", newfile, function(err){
+         if(err) throw err;
+      })
+   });
+
 }
